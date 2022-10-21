16f23f1ddf1579cbf24d0344e64ce1296d209464
selmachaoui <99722260+selmachaoui@users.noreply.github.com>
2022-10-21 23:17:08 +0200

    Add files via upload

---
+++ Code R/documentationfonctionnelle.R
@@ -0,0 +1,18 @@
+# Prérequis
+Documentation fonctionnelle :
+  -R+RStudio
+- Pour réaliser le projet, nous avons utilisé plusieurs packages. Le package DT, ggplot2, colourpicker, plotly,googlevis 
+- Nous avons ensuite importé les données nécessaires à la création de graphiques.  
+-Nous choisissons une palette de couleurs.
+- Nous avons installé les librairies shiny, shinydashboard, esquisse, ggplot2 et dplyr.
+Dans le serveur, nous avons exécuté les tâches suivantes :
+  Nous avons construit plusieurs documents pour répondre au sujet. Sur Github, on retrouve l’application RShiny, RMarkdown, 
+#La problématique : Nous avons décidé de centrer le projet sue l’étude des influenceurs instagram
+C’est pourquoi nous avons décidé de choisir la problématique : « Qu’est-ce qui détermine la popularité d’un influenceur ? »
+
+# La connexion à la base de données :
+Nous avons téléchargé les données Instagram sous format « Excel » puis ajouté la colonne « Continent ». A l’aide des « Recherche V » , nous avons crée des autres tables, ensuite converties en CSV pour le traitement RShiny. 
+
+# Les conclusions à faire ressortir :
+
+Dans les volets, les graphiques et statistiques apportent des réponses en lien avec la problématique. 
